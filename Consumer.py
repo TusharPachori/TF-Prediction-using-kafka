81abfdb98e8b6d35c3add9c451c354fdde94292d
Tushar Pachori <tusharpachori29061997@gmail.com>
2021-07-10 15:23:24 +0530

    Show image

--- Consumer.py
+++ Consumer.py
@@ -1,10 +1,12 @@
 from kafka import KafkaConsumer
 from json import loads
 import cv2
+import base64
+import numpy as np
 
 
 consumer = KafkaConsumer(
-    'test1',
+    'test10',
      bootstrap_servers=['localhost:9092'],
      auto_offset_reset='earliest',
      enable_auto_commit=False,
@@ -12,4 +14,8 @@ consumer = KafkaConsumer(
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
 for message in consumer:
-    print('message recieved')
+    jpg_original = base64.b64decode(message.value['frame'])
+    jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+    img = cv2.imdecode(jpg_as_np, flags=1)
+    cv2.imshow("video", img)
+    # print('{}'.format(message.value[frame]))
--- Producer.py
+++ Producer.py
@@ -1,20 +1,24 @@
-from time import sleep
 from json import dumps
 from kafka import KafkaProducer
 import cv2
 import json
+import base64
+
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: 
                          dumps(x).encode('utf-8'))
 
 webcam = cv2.VideoCapture("/Users/tusharpachori/Downloads/input.mp4")
+webcam.set(cv2.CAP_PROP_FPS, 1)
 
 while(webcam.isOpened()):
     _, frame = webcam.read()
     if(_ == True):
-        data = {'frame' : frame.tolist()}
-        producer.send('test1', value=data)
+        jpg_as_text = base64.b64encode(cv2.imencode('.jpg', frame)[1]).decode()
+        data = {'frame' : jpg_as_text}
+        producer.send('test10', value=data)
+        cv2.imshow("video", frame)
     else:
         break
 
