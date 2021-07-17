89f378a15cc64795e98d7c514ec8a17fcf9b3a35
Shubhank Tyagi <shubhank.tyagi@tipotapp.com>
2021-07-17 13:04:39 +0530

    prod cons setted up

--- Consumer.py
+++ Consumer.py
@@ -1,55 +1,53 @@
+#!/Users/shubhanktyagi/opt/anaconda3/envs/cvrms/bin/python
 from kafka import KafkaConsumer, TopicPartition
-from json import loads
 import cv2
 import base64
+import argparse
 import numpy as np
+from json import loads
 
-TOPIC = "test23"
-PARTITION_0 = 0
-PARTITION_1 = 1
+def get_args():
+    parser = argparse.ArgumentParser(description='PyTorch Detection Training', add_help=True)
+    parser.add_argument('--topic', default='test100', help='topic')
+    return parser
 
-# consumer_0 = KafkaConsumer(
-#     TOPIC, group_id='my-group-2', bootstrap_servers=['10.50.23.120:9092']
-# )
-# consumer_1 = KafkaConsumer(
-#     TOPIC, group_id='my-group-2', bootstrap_servers=['10.50.23.120:9092']
-# )
+if __name__ == "__main__":
+    args = get_args().parse_args()
+    # TOPIC = "test23"
+    # PARTITION_0 = 0
+    # PARTITION_1 = 1
 
-
-consumer = KafkaConsumer(
-    # 'test22',
-     bootstrap_servers=['localhost:9092'],
-     auto_offset_reset='earliest',
-     group_id='my-group-2',
-     value_deserializer=lambda x: loads(x.decode('utf-8')))
-
-topic_partition_0 = TopicPartition(TOPIC, PARTITION_0)
-topic_partition_1 = TopicPartition(TOPIC, PARTITION_1)
-# format: topic, partition
-consumer.assign([topic_partition_0])
-
-# fourcc = cv2.VideoWriter_fourcc('m','p','4', 'v')
-# writer = cv2.VideoWriter("/Users/shubhanktyagi/Desktop/out.mp4" , fourcc,30, (1280,720))
-
-for message in consumer:
-    # if "data" in message.value:
-    #     writer.release()
-    #     break
-    # jpg_original = base64.b64decode(message.value['frame'])
-    # jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
-    # img = cv2.imdecode(jpg_as_np, flags=1)
-    # cv2.imshow("video", img)
-    # writer.write(img)
-    print('{}'.format(message.value["frame"]))
+    # consumer_0 = KafkaConsumer(
+    #     TOPIC, group_id='my-group-2', bootstrap_servers=['10.50.23.120:9092']
+    # )
+    # consumer_1 = KafkaConsumer(
+    #     TOPIC, group_id='my-group-2', bootstrap_servers=['10.50.23.120:9092']
+    # )
 
 
-# from kafka import KafkaConsumer
-# from kafka.structs import TopicPartition
+    consumer = KafkaConsumer(
+        args.topic,
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        group_id='my-group-2',
+        enable_auto_commit = False,
+        value_deserializer=lambda x: loads(x.decode('utf-8')))
 
-# topic = 'test22'
-# bootstrap_servers = 'localhost:9092'
-# consumer = KafkaConsumer(
-#     topic, bootstrap_servers=bootstrap_servers, auto_offset_reset='earliest')
+    # topic_partition_0 = TopicPartition(TOPIC, PARTITION_0)
+    # topic_partition_1 = TopicPartition(TOPIC, PARTITION_1)
+    # # format: topic, partition
+    # consumer.assign([topic_partition_0])
 
-# partitions = consumer.partitions_for_topic(topic)
-# print(partitions)
+    fourcc = cv2.VideoWriter_fourcc('m','p','4', 'v')
+    writer = cv2.VideoWriter("/Users/shubhanktyagi/Desktop/out.mp4" , fourcc,30, (1280,720))
+
+    for message in consumer:
+        print("Writing frame: ", message.value["count"], end="\r")
+        if message.value["count"] <=0:
+            writer.release()
+            break
+        jpg_original = base64.b64decode(message.value['frame'])
+        jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
+        img = cv2.imdecode(jpg_as_np, flags=1)
+        # cv2.imshow("video", img)
+        writer.write(img)
--- Producer.py
+++ Producer.py
@@ -1,48 +1,52 @@
+#!/Users/shubhanktyagi/opt/anaconda3/envs/cvrms/bin/python
+import argparse
 from json import dumps
 from kafka import KafkaProducer, KafkaAdminClient
 from kafka.admin import NewPartitions, NewTopic
 import cv2
 import json
 import base64
-from time import sleep
+import time
 
+def get_args():
+    parser = argparse.ArgumentParser(description='PyTorch Detection Training', add_help=True)
+    parser.add_argument('--input-video', default='/Users/shubhanktyagi/Downloads/open_drill_11.mp4', help='input video')
+    parser.add_argument('--topic', default='test100', help='topic')
+    return parser
 
-topic = 'test23'
-bootstrap_servers = 'localhost:9092'
+if __name__ == "__main__":
+    args = get_args().parse_args()
+    topic = args.topic
+    bootstrap_servers = 'localhost:9092'
 
-admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
-topic_list = []
-topic_list.append(NewTopic(name=topic, num_partitions=2, replication_factor=1))
-try:
-    admin_client.create_topics(new_topics=topic_list, validate_only=False)
-except:
-    print()
-# topic_partitions = {}
-# topic_partitions[topic] = NewPartitions(total_count=2)
-# admin_client.create_partitions(topic_partitions)
+    # admin_client = KafkaAdminClient(bootstrap_servers=bootstrap_servers)
+    # topic_list = []
+    # topic_list.append(NewTopic(name=topic, num_partitions=2, replication_factor=1))
+    # try:
+    #     admin_client.create_topics(new_topics=topic_list, validate_only=False)
+    # except:
+    #     print()
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
-                         value_serializer=lambda x: 
-                         dumps(x).encode('utf-8'))
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                            value_serializer=lambda x: 
+                            dumps(x).encode('utf-8'))
 
-# webcam = cv2.VideoCapture("/Users/shubhanktyagi/Downloads/open_drill_11.mp4")
-# webcam.set(cv2.CAP_PROP_FPS, 1)
+    webcam = cv2.VideoCapture(args.input_video)
+    webcam.set(cv2.CAP_PROP_FPS, 1)
+    ctr = 1
 
-# while(webcam.isOpened()):
-ctr = 0
-while(True):
-    print(ctr)
-    producer.send(topic, value={"frame": ctr})
-    sleep(0.3)
-    ctr+=1
-    # _, frame = webcam.read()
-    # if(_ == True):
-    #     jpg_as_text = base64.b64encode(cv2.imencode('.jpg', frame)[1]).decode()
-    #     data = {'frame' : jpg_as_text}
-    #     producer.send('test11', value=data)
-    #     # cv2.imshow("video", frame)
-    # else:
-    #     producer.send('test11', value={'data': "STOP"})
-    #     break
-
-# webcam.release()
+    while(webcam.isOpened()):
+    # while(True):
+        _, frame = webcam.read()
+        if(_ == True):
+            jpg_as_text = base64.b64encode(cv2.imencode('.jpg', frame)[1]).decode()
+            data = {"count": ctr, "frame" : jpg_as_text}
+            ctr += 1
+            producer.send(topic, value=data)
+            # cv2.imshow("video", frame)
+        else:
+            print("send")
+            producer.send(topic, value={"count": -1, "frame": ""})
+            # producer.send(topic, value={"count": -2, "frame": ""})
+            webcam.release()
+            break
